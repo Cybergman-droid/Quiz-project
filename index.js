53ee59599239cb042cea6781e61a8bf711b89f62
Cybergman-droid <jasongakpo20@gmail.com>
2025-09-30 13:31:24 +0000

    Added local storage for the question se;ector

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function getData() {
+    localStorage.clear()
     username = document.getElementById("username").value;
     questionNum = document.getElementById("questionNum").value;
-    localStorage.setItem("username", username);
-    localStorage.setItem("questionNum", questionNum);   
-}
+    physics = document.getElementById('phy').checked
+    biology = document.getElementById('bio').checked
+    chemistry = document.getElementById('chem').checked
+    compsci = document.getElementById('compSci').checked
 
+    localStorage.setItem("username", username);
+    localStorage.setItem("questionNum", questionNum); 
+    localStorage.setItem('phy', physics)  
+    localStorage.setItem('bio', biology)  
+    localStorage.setItem('chem', chemistry)  
+    localStorage.setItem('compSci', compsci)
+}
 
 
 let submitBtn = document.getElementById('submit')
